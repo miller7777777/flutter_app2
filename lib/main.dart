3777836602f99b453052236affd745fed1a57d1b
Miller777 <miller777@mail.ru>
2020-11-12 02:34:54 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Second App"),
+        backgroundColor: const Color(0xFF509f93),
+        centerTitle: true,
+      ),
+      body: Stack(
+        children: [
+          Text(
+            "А куда ты хочешь попасть?",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 30, fontFamily: "OpenSans"),
+          ),
+          Center(
+            child: Image(
+              image: AssetImage("assets/images/cat.png"),
+            ),
+          ),
+          Align(alignment: Alignment.bottomCenter),
+          
+
+          ])
+        ],
+      ),
+    ),
+  ));
+}
